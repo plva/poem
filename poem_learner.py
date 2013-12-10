2b32d2e2c34ef7464ff0c3ccf82590aed18ebe24
Paul Vasiu <paul.vasiu@gmail.com>
2013-12-10 11:42:06 -0500

    fixed it up a bit

--- poem_learner.py
+++ poem_learner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/Python
 from getch import getch
 from sys import argv, stdin, stdout, exit
-import os
+from os import system
 
 
 # return lines from file
@@ -16,6 +16,7 @@ def get_poem_line_list(filename):
 
 class PoemRunner:
     """Creates PoemLine objects out of lines and controls the top level running"""
+
     def __init__(self, line_list):
         self.line_list = line_list
         self.poemlines = []
@@ -26,6 +27,7 @@ class PoemRunner:
         self.startLearn()
 
     def makeLines(self):
+        # I wonder if there is a more Pythonic way of doing this..
         first = self.line_list[0].strip()
         for line in [line.strip() for line in self.line_list if line.strip()][1:]:
             p = PoemLine(first, line)
@@ -69,10 +71,12 @@ class PoemLine:
     
     def ask(self):
         i = 0
+        # Don't think system actually throws an error
+        # Might not work under Windows.
         try: 
-            os.system('clear')
+            system('clear')
         except:
-            os.system('cls')
+            system('cls')
         print self.line
 
         while i < len(self.nextline):
@@ -117,7 +121,7 @@ class PoemLine:
         # if code reaches here, the prompt is at the end of the current line.
         # So, the functionality here may be a bit different than when in the middle of the line.
         
-        # if f is pressed, don't go on to the next line
+        # if f is pressed once more, don't go on to the next line
         a = getch()
         while a.lower() not in 'anq':
             a = getch()
@@ -129,32 +133,23 @@ class PoemLine:
             # not really a quit here, but two q's in a row are intuitive
             elif a == 'q':
                 return 0
-        # just in case something isn't really caught
+        # Have to make sure we never return NULL (can't do arithmetic with it)
+        # Returning 0 just repeats the ask()
         return 0
 
-    def __repr__(self):
-        return self.line
-
-for e in argv:
-    print type(e)
-
-# haven't included this. 
-if len(argv) == 2:
-    filename = argv[1]
-elif len(argv) == 3:
-    pass
-else:
-    filename = ''
-    # INPUT = stdin.read()
-    # print INPUT
 
 
-
-
-
-g = get_poem_line_list(filename)
-
-p = PoemRunner(g)
-
+if __name__ == "__main__":
+    # If no file is passed, ask for it
+    if len(argv) == 2:
+        filename = argv[1]
+    else:
+        filename = ''
+        # INPUT = stdin.read()
+        # print INPUT
+            
+    g = get_poem_line_list(filename)
+    p = PoemRunner(g)
+        
 
 
